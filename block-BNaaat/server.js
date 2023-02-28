e0ec309118fdc174a257106059b1caa31825650f
ANISH KUMAR GIRI <girianish2004@gmail.com>
2023-02-28 20:48:35 +0530

    adding content to my url

--- block-BNaaat/server.js
+++ block-BNaaat/server.js
@@ -2,6 +2,13 @@ let http = require("http");
 
 let fs = require("fs");
 http.createServer((req,res) => {
+    if(req.url == "/"){
+        res.end("welcome anish")
+    }else if(req.url == "/about"){
+        fs.createReadStream("./index.html").pipe(res);
+    }
     
+}).listen(3000,()=> {
+    console.log("We are creating a server")
 })
 
